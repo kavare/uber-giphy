17df9f2caef1410da39b297456e1a68a42138d93
kavare <aarontthsieh@gmail.com>
2019-12-24 09:35:58 +0800

    Updates

---
+++ precache-manifest.2eef181b84e077aa3e81b9058bf17fd9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e63b023d17f6c4ca9175a287ed9ea03",
+    "url": "/uber-giphy/index.html"
+  },
+  {
+    "revision": "7e99712b5f848011d931",
+    "url": "/uber-giphy/static/css/main.37613161.chunk.css"
+  },
+  {
+    "revision": "e4329fd8b31efd073237",
+    "url": "/uber-giphy/static/js/2.1a876ad3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/uber-giphy/static/js/2.1a876ad3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e99712b5f848011d931",
+    "url": "/uber-giphy/static/js/main.f7c0e744.chunk.js"
+  },
+  {
+    "revision": "10784522a7571b091f84",
+    "url": "/uber-giphy/static/js/runtime-main.d07d763b.js"
+  },
+  {
+    "revision": "04e09deee72d5fce182103961d90edb8",
+    "url": "/uber-giphy/static/media/logo.04e09dee.svg"
+  },
+  {
+    "revision": "e7d93e1827e3d1a6ea962012fe54abda",
+    "url": "/uber-giphy/static/media/single-column.e7d93e18.svg"
+  },
+  {
+    "revision": "ba5f98353ad2ca3039df1021d8a4341c",
+    "url": "/uber-giphy/static/media/three-columns.ba5f9835.svg"
+  }
+]);
